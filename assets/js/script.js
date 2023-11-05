a5d0998d99cc7b0f66ce94f83f929a06cdbd12db
Franco Roldão <francotrabalhoestudo@gmail.com>
2023-11-05 16:19:14 -0300

    Seção inputs Treinamento e personalização scrollBar
    index.html -> Inicio da criação da seção "inputs", onde serão exibidos inputs html disponíveis para treinamento.
    script.js -> criação das funções que informam as opções selecionadas nos inputs radio - checkbox e date.
    elements.css -> personalização da scroll bar da página.
    classesReutilizaveis.css -> estilização dos botões exibidos dentro da seção de treinamento de inputs ->>estilização da div em que os inputs estão inseridos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,54 @@ function exibirImagem(text) {
     galleryText.style.display = 'none';
 }
 
-//alert personalizado ao enviar formulário:
+//alert ao enviar form do 'inputsTreinamento' com radio button marcado ou nenhuma opção marcada:
+
+function informarOpcoesSelecionadas(tipoInput) {
+   
+  let input = "";
+  
+  if(tipoInput === "checkbox"){
+    input = document.getElementsByName("inputCheckbox");
+  } else{
+    input = document.getElementsByName("inputRadio1");
+  }
+    let opcoesSelecionadas = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i].checked) {
+            opcoesSelecionadas.push(input[i].value);
+        }
+    }
+
+    if (opcoesSelecionadas.length > 0) {
+        var mensagem = "Você marcou: " + opcoesSelecionadas;
+        alert(mensagem);
+    } else {
+        alert("Nenhuma opção marcada!");
+    }
+}
+
+// função para informar data selecionada no input date - inputs treinamento.
+
+function informarDataSelecionada(){
+
+    let dataSelecionada = document.getElementById("inputData").value;
+
+
+    if(dataSelecionada === ""){
+
+        alert("Nenhuma data selecionada!");
+    }else{
+        alert("Data selecionada -> " + dataSelecionada);
+    }
+}
+
+
+
+
+
+
+
+
+
 
